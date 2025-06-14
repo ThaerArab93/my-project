9eacb680a5f3a3c144baf923c87d46ac27ca590e
ThaerArab93 <thaer93arab@gmail.com>
2025-06-14 14:40:48 +0300

    feat: create layout with navbar and footer, add Bootstrap 5, Google Fonts, jQuery, and custom CSS

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Cairo Font "Google Font Link" -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap 5 CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Custom CSS File -->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom-style.css') }}">
+
+    <title>{{ $title ?? 'Thaer Arab | Résumé' }}</title>
+</head>
+<body>
+    @include('partials.navbar')
+    <main>
+        @yield('content')
+    </main>
+    @include('partials.footer')
+
+    <!-- Jquery -->
+    <script src="{{ asset('assets/js/jquery-3.6.4.min.js') }}"></script>
+    <!-- Bootstrap 5 Bundle CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
